0585e673d9c35246bed54179f0c7518f2873d034
ShokoEnya <enyashoko@gmail.com>
2020-12-22 16:43:07 +0900

    単体テストコード"passwordが英字のみだと登録できない"追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -40,8 +40,13 @@ RSpec.describe User, type: :model do
       @user.valid?
       expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
     end
-    it 'パスワードが半角英数字混合でないと登録できないこと' do
+    it 'パスワードが半角数字のみでは登録できないこと' do
       @user.password = '000000'
+      @user.valid?
+      expect(@user.errors.full_messages).to include('Password には英字と数字の両方を含めて設定してください')
+    end
+    it 'パスワードが半角英字のみでは登録できないこと' do
+      @user.password = 'aaaaaa'
       @user.valid?
       expect(@user.errors.full_messages).to include('Password には英字と数字の両方を含めて設定してください')
     end
