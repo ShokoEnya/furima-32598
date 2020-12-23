ea825a2a1a2c89d2693b653a4083bdad73616534
ShokoEnya <enyashoko@gmail.com>
2020-12-23 12:35:58 +0900

    「配送料の負担」のActive Hashの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,9 +6,11 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :status
+  belongs_to :delivery_fee
 
   with_options presence: true, numericality: { other_than: 1 } do
     validates :category_id
     validates :status
+    validates :delivery_fee
   end
 end
