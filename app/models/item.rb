680041c87a5687bb7766f1c851ed4063aa9dbff7
ShokoEnya <enyashoko@gmail.com>
2020-12-23 13:02:37 +0900

    「発送元の地域」のActive Hashの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,10 +7,12 @@ class Item < ApplicationRecord
   belongs_to :category
   belongs_to :status
   belongs_to :delivery_fee
+  belongs_to :Prefecture
 
   with_options presence: true, numericality: { other_than: 1 } do
     validates :category_id
     validates :status
     validates :delivery_fee
+    validates :Prefecture
   end
 end
