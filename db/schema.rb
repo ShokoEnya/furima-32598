3c6a2660eeda305ca614426986b66b612492fb36
ShokoEnya <enyashoko@gmail.com>
2020-12-24 19:26:39 +0900

    create_itemsマイグレーションファイルに"null: false"を追加

--- db/schema.rb
+++ db/schema.rb
@@ -34,14 +34,14 @@ ActiveRecord::Schema.define(version: 2020_12_22_093135) do
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "product_name"
-    t.text "description"
-    t.integer "category_id"
-    t.integer "status_id"
-    t.integer "delivery_fee_id"
-    t.integer "prefecture_id"
-    t.integer "duration_id"
-    t.integer "price"
+    t.string "product_name", null: false
+    t.text "description", null: false
+    t.integer "category_id", null: false
+    t.integer "status_id", null: false
+    t.integer "delivery_fee_id", null: false
+    t.integer "prefecture_id", null: false
+    t.integer "duration_id", null: false
+    t.integer "price", null: false
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
