70aef5b3b0de748a9de936177037eaba3c5ca1d8
ShokoEnya <enyashoko@gmail.com>
2020-12-26 17:31:38 +0900

    items_controllerのリファクタリング2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :find_item, only: [:show, :edit, :update, :move_to_index]
   before_action :move_to_index, only: [:edit, :update]
-  before_action :find_item, only: [:show, :edit, :update]
 
 
   def index
@@ -42,12 +42,11 @@ class ItemsController < ApplicationController
                                  :prefecture_id, :duration_id, :price).merge(user_id: current_user.id)
   end
 
-  def move_to_index
-    @item = Item.find(params[:id])
-    redirect_to root_path unless current_user.id == @item.user_id
-  end
-
   def find_item
     @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    redirect_to root_path unless current_user.id == @item.user_id
   end
 end
