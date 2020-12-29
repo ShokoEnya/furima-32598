b3a80609419d44be37386fee59b722507b546791
ShokoEnya <enyashoko@gmail.com>
2020-12-29 16:59:59 +0900

    ordersのビューの実装

--- app/models/user_order.rb
+++ app/models/user_order.rb
@@ -1,7 +1,9 @@
 class UserOrder
   include ActiveModel::Model
-  attr_accessor :post_code, :prefecture_id, :city, :addresses, :building, :phone_no
+  attr_accessor :post_code, :prefecture_id, :city, :addresses, :building, :phone_no, :token
 
+  belongs_to :prefecture
+  
   with_options presence: true do
     validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)." }
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
