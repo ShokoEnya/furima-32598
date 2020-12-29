23dfc48693a6afbc57973505771b0cb913509ba8
ShokoEnya <enyashoko@gmail.com>
2020-12-29 14:55:42 +0900

    バリデーションの設定

--- app/models/user_order.rb
+++ app/models/user_order.rb
@@ -1,3 +1,13 @@
 class UserOrder
   include ActiveModel::Model
+  attr_accessor :post_code, :prefecture_id, :city, :addresses, :building, :phone_no
+
+  with_options presence: true do
+    validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)." }
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :city
+    validates :addresses
+    validates :phone_no, numericality: { only_integer: true, message: "is invalid. Input only numbers." }
+    validates :token
+  end
 end
